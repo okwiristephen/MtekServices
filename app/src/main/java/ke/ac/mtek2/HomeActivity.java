249e4a062b5a87703f774d029f3f908a58a7b96c
okwiristephen <okwirialal@gmail.com>
2019-12-17 12:34:33 +0300

    git init git add . git commit -m "origin master" git push -u origin master git commit "final commit"

--- app/src/main/java/ke/ac/mtek2/HomeActivity.java
+++ app/src/main/java/ke/ac/mtek2/HomeActivity.java
@@ -1,6 +1,7 @@
 package ke.ac.mtek2;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -10,12 +11,19 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import ke.ac.mtek2.ui.login.EmegencyFragment;
+import ke.ac.mtek2.ui.login.FrSettings;
+import ke.ac.mtek2.ui.login.NotificationFragment;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ActionBar actionBar = getSupportActionBar();
+        
 
         BottomNavigationView navigationView = findViewById(R.id.btm_nav);
         navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -29,8 +37,32 @@ public class HomeActivity extends AppCompatActivity {
                     fragmentTransaction.replace(R.id.frame_layout, fragment);
                     fragmentTransaction.commit();
                 }
+                if (id == R.id.settings){
+                    FrSettings fragment = new FrSettings();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }if (id == R.id.emergency){
+                    EmegencyFragment fragment = new EmegencyFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }if (id == R.id.notification){
+                    NotificationFragment fragment = new NotificationFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
+                if (id == R.id.home){
+                    HomeFragment fragment = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
                 return true;
             }
         });
+        navigationView.setSelectedItemId(R.id.home);
+
     }
     }
